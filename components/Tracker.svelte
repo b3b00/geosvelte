82ea45c05e973c44c97acd277c864348b63336e8
b3b00 <olivier.duhart@gmail.com>
2024-01-31 18:28:30 +0100

    export gpx

--- components/Tracker.svelte
+++ components/Tracker.svelte
@@ -2,6 +2,7 @@
 
     import { onMount } from 'svelte';
     import {positions} from '../scripts/store';
+    import { format } from "date-fns";
     
     $:{        
     }
@@ -32,8 +33,35 @@
     function tick(position:GeolocationPosition) {
         const newData = $positions;
         newData.push(position);
+        
         $positions = newData;
     }
+
+
+    function trackPoint(position:GeolocationPosition ) : string{
+        const dt = format(new Date(position.timestamp), "yyyy-MM-dd'T'HH:mm:ss'Z'");
+        const trkpt=`<trkpt lat="${position.coords.latitude}" lon="${position.coords.longitude}">
+    <ele>71.7</ele>
+    <time>${dt}</time>    
+   </trkpt>`;
+   return trkpt
+    }
+
+    let header = (d) => {
+        return `<?xml version="1.0" encoding="UTF-8"?>
+<gpx creator="StravaGPX" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd" version="1.1" xmlns="http://www.topografix.com/GPX/1/1" xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1" xmlns:gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3">
+ <metadata>
+  <time>${format(new Date(d), "yyyy-MM-dd'T'HH:mm:ss'Z'")}</time>
+ </metadata>
+ <trk>
+  <name>Lunch Run</name>
+  <type>running</type>
+  <trkseg>`;
+  }
+
+  let footer = `</trkseg>
+ </trk>
+</gpx>`;
 
     function stop() {
         // TODO compute gpx ?
@@ -43,9 +71,9 @@
         recording = false;
         const j = JSON.stringify($positions);
 
-        var pos:string[] = $positions.map(x => {
-            return `lat:${x.coords.latitude} lon:${x.coords.longitude}`;
-        });
+        
+
+        var pos:string[] = $positions.map(trackPoint);
         const poslist = pos.join('\n');
 
         for(let i = 0; i < $positions.length; i++)  {
@@ -53,10 +81,36 @@
             console.log(p);
         }
         console.log(poslist);
-        gpx = poslist;
+        gpx = header($positions[0].timestamp)+'\n'+poslist+'\n'+footer;
     }
 
+    function downloadGPS() {
+        const blob = new Blob([gpx], {  type: "text/plain"});
+        const url = URL.createObjectURL(blob);
+        
+        const link = document.createElement("a");
 
+  // Set link's href to point to the Blob URL
+  link.href = url;
+  link.download = "track.gpx";
+
+  // Append link to the body
+  document.body.appendChild(link);
+
+  // Dispatch click event on the link
+  // This is necessary as link.click() does not work on the latest firefox
+  link.dispatchEvent(
+    new MouseEvent('click', { 
+      bubbles: true, 
+      cancelable: true, 
+      view: window 
+    })
+  );
+
+  // Remove link from body
+  document.body.removeChild(link);
+
+    }
 
     
 
@@ -71,6 +125,9 @@
     <p>recording... {$positions.length} points</p>
     <button on:click={stop}>stop</button>
     {/if}
-    <pre>{gpx}</pre>
+    {#if gpx && gpx.length > 0} 
+        <button on:click={downloadGPS}>download</button>
+        <pre>{gpx}</pre>
+    {/if}
     
 </div>
